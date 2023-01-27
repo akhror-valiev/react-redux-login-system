286790e6b0bb0203b8f0f600c840ef0097301176
Oshie <ahror.valiev@gmail.com>
2023-01-28 00:37:21 +0500

    Revert "Add functionality"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { useSelector } from 'react-redux';
 import Login from './components/Login';
-import { selectUser } from './features/userSlice';
-import Logout from './components/Logout';
 
 function App() {
-  const user = useSelector(selectUser);
-
   return (
-    <div>
-      {user ? <Logout /> : <Login />}
+    <div className="App">
+      <Login />
     </div>
   );
 }
